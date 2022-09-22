53821a7b0752de75465e8d4744f5d103bdd633de
Sean Coughlin <coughlinscc33@gmail.com>
2022-09-22 13:58:41 -0500

    Initial Commit

---
+++ Charts-Example/Charts_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Charts_ExampleApp.swift
+//  Charts-Example
+//
+//  Created by Sean Coughlin on 9/22/22.
+//
+
+import SwiftUI
+
+@main
+struct Charts_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Charts-Example/ContentView.swift
@@ -0,0 +1,54 @@
+//
+//  ContentView.swift
+//  Charts-Example
+//
+//  Created by Sean Coughlin on 9/22/22.
+//
+
+import SwiftUI
+import Charts
+
+struct ToyShape: Identifiable {
+    var color: String
+    var type: String
+    var count: Double
+    var id = UUID()
+}
+
+var stackedBarData: [ToyShape] = [
+    .init(color: "Green", type: "Cube", count: 2),
+    .init(color: "Green", type: "Sphere", count: 0),
+    .init(color: "Green", type: "Pyramid", count: 1),
+    .init(color: "Purple", type: "Cube", count: 1),
+    .init(color: "Purple", type: "Sphere", count: 1),
+    .init(color: "Purple", type: "Pyramid", count: 1),
+    .init(color: "Pink", type: "Cube", count: 1),
+    .init(color: "Pink", type: "Sphere", count: 2),
+    .init(color: "Pink", type: "Pyramid", count: 0),
+    .init(color: "Yellow", type: "Cube", count: 1),
+    .init(color: "Yellow", type: "Sphere", count: 1),
+    .init(color: "Yellow", type: "Pyramid", count: 2)
+]
+
+struct ContentView: View {
+    var body: some View {
+        Chart {
+            ForEach(stackedBarData) { shape in
+                BarMark(
+                    x: .value("Shape Type", shape.type),
+                    y: .value("Total Count", shape.count)
+                )
+                .foregroundStyle(by: .value("Shape Color", shape.color))
+            }
+        }
+        .chartForegroundStyleScale([
+            "Green": .green, "Purple": .purple, "Pink": .pink, "Yellow": .yellow
+        ])
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
